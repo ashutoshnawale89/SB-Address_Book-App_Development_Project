4b64194923bebd9c0e4f670043089336d2dbd34a
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-12-21 17:28:18 +0530

    Added:Welcome Message Pring in Console

--- src/main/java/com/bridgelabz/Addressbook/app/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/Addressbook/app/AddressBookAppApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AddressBookAppApplication.class, args);
+		System.out.println("Welcome To Address Book App ");
 	}
 
 }
